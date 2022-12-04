fa65624a2142f725209ff8e0cdf329c3796b878a
Christopher Jones <chris.jones120@outlook.com>
2022-12-04 11:02:11 +1000

    Play around with types

--- 3/main.hs
+++ 3/main.hs
@@ -10,7 +10,7 @@ main = do
     [path] -> do
       contents <- readFile path
       putStrLn "Incorrectly sorted items: "
-      print $ sum $ map (priority . sharedItems . rucksack) $ lines contents
+      print $ sum $ map (priority . sharedItem . rucksack) $ lines contents
       putStrLn "\n Group badges: "
       print $ sum $ map priority . badges $ map rucksack $ lines contents
     _ -> putStrLn "Please provide a path to the input file."
@@ -21,22 +21,25 @@ rucksack s = splitAt (length s `div` 2) s
 uniq :: [Char] -> [Char]
 uniq = map head . group
 
-sharedItems :: Rucksack -> [Char]
-sharedItems (a, b) = uniq $ filter (`elem` b) a
+sharedItem :: Rucksack -> Char
+sharedItem (a, b) = i
+  where
+    [i] = uniq $ filter (`elem` b) a
 
-groupBadge :: Rucksack -> Rucksack -> Rucksack -> [Char]
-groupBadge a b c =
-  uniq $ filter (`elem` all' c) $ filter (`elem` all' b) $ all' a
+groupBadge :: Rucksack -> Rucksack -> Rucksack -> Char
+groupBadge a b c = i
+  where
+    [i] = uniq $ filter (`elem` all' c) $ filter (`elem` all' b) $ all' a
 
-priority :: [Char] -> Int
-priority [c] = i + 1
+priority :: Char -> Int
+priority c = i + 1
   where
     Just i = c `elemIndex` (['a' .. 'z'] ++ ['A' .. 'Z'])
 
 all' :: Rucksack -> [Char]
 all' (l, r) = l ++ r
 
-badges :: [Rucksack] -> [[Char]]
+badges :: [Rucksack] -> [Char]
 badges r = f r []
   where
     f [] agg = agg
